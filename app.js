7584ccbc76567e07cbc4f54ba87e6448d8cb3edd
roopeshbounce <roopesh.lr@bounceshare.com>
2020-05-19 08:55:27 +0530

    added route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 
-app.get("/",(req,res)=>{
+app.get("/roopz",(req,res)=>{
 res.send("ghsdkhasd");
 })
 const port = process.env.port||3000;
