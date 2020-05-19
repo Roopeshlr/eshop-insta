138537ace34f1bd405dfd5b73ef439bdac36f8b4
roopeshbounce <roopesh.lr@bounceshare.com>
2020-05-19 08:49:36 +0530

    changes url

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 
-app.get("/roope",(req,res)=>{
+app.get("/",(req,res)=>{
 res.send("ghsdkhasd");
 })
 const port = process.env.port||3000;
