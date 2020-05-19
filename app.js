914916245d008c30e18b606b796d1b9b82d7c38b
roopeshbounce <roopesh.lr@bounceshare.com>
2020-05-19 10:45:35 +0530

    added mock data

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
  
 const app = express();
+var mock =
+{
+  "status": "Success",
+  "message": "User Referral reminder list and earning details",
+  "data": {
+    "total_referral_earnings": 250,
+
+    "pending_invites": [
+      {
+        "mobile": "+91-9888888888"
+      },
+      {
+        "mobile": "+91-9888888888"
+      }
+    ]
+  }
+};
+
 
 
 app.get("/roopz",(req,res)=>{
-res.send("ghsdkhasd");
+res.send(mock);
 })
 const port = process.env.port||3000;
 app.listen(port,()=>{
